a35bd6bd7ab49fafc9370ccb6bb62c43e108c5a8
rookie738 <249928269+rookie738@users.noreply.github.com>
2026-05-31 15:28:35 +0800

    完成文件上传功能

--- sky-server/src/main/java/com/sky/config/AliOssConfig.java
+++ sky-server/src/main/java/com/sky/config/AliOssConfig.java
@@ -1,4 +1,18 @@
 package com.sky.config;
 
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AliOssConfig {
+    //创建AliOssUtil对象
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        return new AliOssUtil(aliOssProperties.getEndpoint(),aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),aliOssProperties.getBucketName());
+    }
 }
--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,4 +1,39 @@
 package com.sky.controller.admin;
 
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
 public class CommonController {
+
+    @Autowired
+    AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file){
+        String originalFilename = file.getOriginalFilename();
+        String extensionName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String id = UUID.randomUUID().toString();
+        StringBuffer url = new StringBuffer(id);
+        url.append(extensionName);
+        try {
+            aliOssUtil.upload(file.getBytes(),url.toString());
+            return Result.success(url.toString());
+        } catch (IOException e) {
+            log.info("文件上传失败");
+            return Result.error(MessageConstant.UPLOAD_FAILED);
+        }
+    }
 }
